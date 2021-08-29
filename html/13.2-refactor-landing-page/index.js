e056f517e26ff94178fbb4c801b4e7022c27907a
omri zaher <omriza5@gmail.com>
2021-08-29 16:24:20 +0300

    add: exercise 15.1

---
+++ html/13.2-refactor-landing-page/index.js
@@ -0,0 +1,7 @@
+/** hamburger click event listener */
+
+const hamIcon = document.getElementById("hamIcon");
+hamIcon.addEventListener("click", () => {
+  return console.log("Hello World!");
+});
+console.log(hamIcon);
