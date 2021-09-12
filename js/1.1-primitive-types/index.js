113d9108141c7e2a82975b848f741b84c6b7ddb6
omri zaher <omriza5@gmail.com>
2021-09-12 09:41:25 +0300

    add: exercise 1.1 solution

---
+++ js/1.1-primitive-types/index.js
@@ -0,0 +1,17 @@
+let str = "Hello World";
+let num = 10;
+
+let obj = {
+  name: "omri zaher",
+  age: 31,
+};
+
+let isOpen = false;
+
+let nullValue = null;
+
+let undefinedValue;
+
+let symbol = Symbol("foo");
+
+console.log(typeof symbol);
