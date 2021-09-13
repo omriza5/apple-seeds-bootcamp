d4b5850ddd5b7d7f53f92b975976c3c82f542fdf
omri zaher <omriza5@gmail.com>
2021-09-13 12:35:28 +0300

    modify: exercise 4.3 solution

--- js/4.3-basketball/index.js
+++ js/4.3-basketball/index.js
@@ -1,5 +1,22 @@
 const johnsTeamScores = [89, 120, 103];
 const mikesTeamScores = [116, 94, 123];
+const maryTeamScores = [79, 134, 105];
+
+function displayWinner(johnsTeamScores, mikesTeamScores, maryTeamScores) {
+  const johnsTeamAvg = scoresAvg(johnsTeamScores);
+  const mikesTeamAvg = scoresAvg(mikesTeamScores);
+  const maryTeamAvg = scoresAvg(maryTeamScores);
+
+  const maxAvg = getMaxAvg(johnsTeamAvg, mikesTeamAvg, maryTeamAvg);
+
+  if (maxAvg === johnsTeamAvg)
+    console.log("Johns Team is The Winner with Average of:", johnsTeamAvg);
+  else if (maxAvg === mikesTeamAvg)
+    console.log("Mikes Team is The Winner with Average of:", mikesTeamAvg);
+  else if (maxAvg === maryTeamAvg)
+    console.log("Mary Team is The Winner with Average of:", maryTeamAvg);
+  else console.log("Its A Draw:", johnsTeamAvg);
+}
 
 function scoresAvg(scores) {
   const sumOfScores = scores.reduce((acc, currentValue) => acc + currentValue);
@@ -7,11 +24,8 @@ function scoresAvg(scores) {
   return sumOfScores / scores.length;
 }
 
-const johnsTeamAvg = scoresAvg(johnsTeamScores);
-const mikesTeamAvg = scoresAvg(mikesTeamScores);
+function getMaxAvg(...args) {
+  return Math.max(...args);
+}
 
-if (johnsTeamAvg > mikesTeamAvg)
-  console.log("Johns Team is The Winner with Average of:", johnsTeamAvg);
-else if (johnsTeamAvg < mikesTeamAvg)
-  console.log("Mikes Team is The Winner with Average of:", mikesTeamAvg);
-else console.log("Its A Draw:", johnsTeamAvg);
+displayWinner(johnsTeamScores, mikesTeamScores, maryTeamScores);
