cd2b15dca5bebc16099b82a361e3928f472cab90
omri zaher <omriza5@gmail.com>
2021-09-14 12:04:15 +0300

    add: 7.3 solution

---
+++ js/7.3-book-utilities/index.js
@@ -0,0 +1,43 @@
+const book1 = {
+  name: "The Lie",
+  author: "Omri Zaher",
+  year: 2020,
+};
+
+const book2 = {
+  name: "The Lie",
+  author: "Omri Zaher",
+  year: 2019,
+};
+
+const bookUtils = {
+  getFirstPublished: function (book1, book2) {
+    return book1.year >= book2.year ? book2 : book1;
+  },
+  setNewEdition: function (book, editionYear) {
+    book.latestEdition = editionYear;
+  },
+  setLanguage: function (book, lang) {
+    book.language = lang;
+  },
+  setTranslation: function (book, lang, translator) {
+    book.translation = {
+      translator,
+      language: lang,
+    };
+  },
+  setPublisher: function (book, name, location) {
+    book.publisher = {
+      name,
+      location,
+    };
+  },
+  isSamePublisher: function (book1, book1, location) {
+    return (
+      book1.publisher.name === book2.publisher.name &&
+      book1.publisher.location === book2.publisher.location
+    );
+  },
+};
+
+console.log(bookUtils.getFirstPublished(book1, book2));
