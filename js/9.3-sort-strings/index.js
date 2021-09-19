5e83197cb6f344b159874ce0c0e3f9d2ed3b0bf4
omri zaher <omriza5@gmail.com>
2021-09-19 16:12:49 +0300

    modify: exercise 9.3

--- js/9.3-sort-strings/index.js
+++ js/9.3-sort-strings/index.js
@@ -18,6 +18,40 @@ const desc = foods.sort((a, b) => {
 
 console.log(desc);
 
+const foodsWithUpperCase = [
+  "falafel",
+  "Sabich",
+  "hummus",
+  "pizza with extra pineapple",
+];
+
+const withCaseSenAsce = (arr) => {
+  return arr.sort((a, b) => {
+    const nameA = a.toLowerCase();
+    const nameB = b.toLowerCase();
+
+    if (nameA > nameB) return 1;
+    if (nameA < nameB) return -1;
+
+    return 0;
+  });
+};
+
+const withCaseSenDesc = (arr) => {
+  return arr.sort((a, b) => {
+    const nameA = a.toLowerCase();
+    const nameB = b.toLowerCase();
+
+    if (nameA > nameB) return -1;
+    if (nameA < nameB) return 1;
+
+    return 0;
+  });
+};
+
+console.log("b1: ", withCaseSenAsce(foodsWithUpperCase));
+console.log("b2: ", withCaseSenDesc(foodsWithUpperCase));
+
 /** c ==> longest word */
 const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
 
