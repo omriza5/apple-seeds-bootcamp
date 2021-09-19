3ee6624be480970154659a846b5ed8d6c24a35f0
omri zaher <omriza5@gmail.com>
2021-09-19 10:19:56 +0300

    modify: exercise 9.2

--- js/9.2-sort-numbers/index.js
+++ js/9.2-sort-numbers/index.js
@@ -3,5 +3,5 @@ const numbers = [1, -5, 666, 2, 400, 11];
 const asce = numbers.sort();
 console.log(asce);
 
-const desc = numbers.reverse();
+const desc = numbers.sort((a, b) => b - a);
 console.log(desc);
