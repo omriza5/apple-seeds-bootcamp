dabd5a3f4e3c3866532e16906eea1fe31edac4e3
omri zaher <omriza5@gmail.com>
2021-09-19 14:26:20 +0300

    modify: exercise 9.1

--- js/9.1-callback-functions/index.js
+++ js/9.1-callback-functions/index.js
@@ -1,9 +1,30 @@
 const isString = (str, cb) => {
   if (typeof str === "string") {
     cb(str);
+  } else {
+    console.log(false);
   }
 };
 
-isString("1", (str) => {
-  console.log(str);
+isString("Omri", () => {
+  console.log(true);
+});
+
+/************************************************************************************** */
+
+const firstWordUpperCase = (sentence, cb) => {
+  const sentenceArr = sentence.split(" ");
+
+  /* Capitalize the first word in the sentence */
+  sentenceArr[0] = sentenceArr[0].toUpperCase();
+
+  /* convert sentence array to a string */
+  const newSentence = sentenceArr.join(" ");
+
+  cb(newSentence);
+};
+
+firstWordUpperCase("Omri Zaher", (sentence) => {
+  const dashedWord = sentence.split(" ");
+  console.log(dashedWord.join("-"));
 });
