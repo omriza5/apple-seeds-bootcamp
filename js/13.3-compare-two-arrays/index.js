ce5e3049ee29a99ab9f996a60d2cc49e6ca53b43
omri zaher <omriza5@gmail.com>
2021-09-20 14:01:02 +0300

    add: 13.3 solution

---
+++ js/13.3-compare-two-arrays/index.js
@@ -0,0 +1,20 @@
+const food = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber", "Olives"];
+const food1 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
+
+function compare(arr1, arr2) {
+  const result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) result.push(arr1[i]);
+    }
+  }
+
+  if (result.length === 0) {
+    return false;
+  }
+
+  return result;
+}
+
+console.log(compare(food, food1));
