d2c928bf9cc45aa1cd68d680604fb6ab4b2353e2
omri zaher <omriza5@gmail.com>
2021-09-22 10:08:09 +0300

    add: 16.1 solution:

---
+++ js/16.1-closures/index.js
@@ -0,0 +1,41 @@
+/** Block 1 */
+
+var b = 1;
+function someFunction(number) {
+  function otherFunction(input) {
+    return b;
+  }
+  b = 5;
+  return otherFunction;
+}
+var firstResult = someFunction(9);
+var result = firstResult(2);
+console.log(result);
+// 5
+
+/** Block 2 */
+var a = 1;
+function b2() {
+  a = 10;
+  return;
+  function a() {}
+}
+b2();
+console.log(a);
+
+//10  ?? ask itay
+
+/** Block 3 */
+let i;
+for (i = 0; i < 3; i++) {
+  const log = () => {
+    console.log("in log");
+    console.log(i);
+  };
+  console.log("out log");
+  setTimeout(log, 100);
+}
+
+// 3
+// 3
+// 3
