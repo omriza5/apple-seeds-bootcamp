f7e5595545aa697465d6db35e9f4dd500bd87c8f
omri zaher <omriza5@gmail.com>
2021-10-04 10:07:13 +0300

    add: 30-2 solution

---
+++ js/03-js-advanced/30.2-promise-chaining/index.js
@@ -0,0 +1,45 @@
+const words = ["hello", "apple", "welcome", "love"];
+const numbers = [1, 2, 3];
+
+function makeAllCaps(words) {
+  return new Promise((resolve, reject) => {
+    if (!isStringArray(words))
+      reject("the contains at least a non string item");
+    else {
+      const capitalizedWords = words.map((word) => word.toUpperCase());
+
+      resolve(capitalizedWords);
+    }
+  });
+}
+function sortWords(words) {
+  return new Promise((resolve, reject) => {
+    if (!isStringArray(words))
+      reject("the contains at least a non string item");
+    else {
+      const sorted = words.sort((a, b) => {
+        if (a > b) return 1;
+        if (a < b) return -1;
+        return 0;
+      });
+
+      resolve(sorted);
+    }
+  });
+}
+
+/** helper function that checks if all array elements are strings */
+function isStringArray(arr) {
+  for (let item of arr) if (typeof item !== "string") return false;
+
+  return true;
+}
+
+/*************************** Main ***********************************/
+makeAllCaps(words)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+sortWords(words)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
