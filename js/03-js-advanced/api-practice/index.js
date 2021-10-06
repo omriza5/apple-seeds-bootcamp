f576f74dd3b6f3d87494df7b9424c1e445dc7ec9
omri zaher <omriza5@gmail.com>
2021-10-06 14:36:36 +0300

    modify: api-practice event handler

--- js/03-js-advanced/api-practice/index.js
+++ js/03-js-advanced/api-practice/index.js
@@ -2,26 +2,26 @@ const container = document.querySelector(".container");
 
 const DOGS_END_POINT = "https://dog.ceo/api/breeds/image/random";
 const CATS_END_POINT = "https://cataas.com/cat?json=true";
-let scroll = 200;
+
+let scroll = 150;
 let lastScrollTop = 0;
 let firstFetch = false;
 let index = 0;
 
+/** on first load */
 if (!firstFetch) {
-  getPetImgs(30, "dog");
+  getPetImgs(25, "dog");
   firstFetch = true;
 }
+
 document.addEventListener("scroll", async (e) => {
   let st = window.pageYOffset || document.documentElement.scrollTop;
 
   if (st > lastScrollTop) {
     if (window.pageYOffset >= scroll) {
-      if (index % 2 === 0) {
-        getPetImgs(5, "cat");
-      } else {
-        getPetImgs(5, "dog");
-      }
-      scroll += 200;
+      index % 2 === 0 ? getPetImgs(5, "cat") : getPetImgs(5, "dog");
+
+      scroll += 150;
       index++;
     }
   }
