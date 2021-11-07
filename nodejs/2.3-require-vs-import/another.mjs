6ea81d2a35f4e479e7f6a1d53dc1930026243172
omri zaher <omriza5@gmail.com>
2021-11-07 12:40:59 +0200

    add: 2.3 solution

---
+++ nodejs/2.3-require-vs-import/another.mjs
@@ -0,0 +1,11 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const div = (a, b) => {
+  return a / b;
+};
+
+export const mult = (a, b) => {
+  return a * b;
+};
---
+++ nodejs/2.3-require-vs-import/index.mjs
@@ -0,0 +1,4 @@
+import { add, mult, div } from "./another.mjs";
+import fs from "fs";
+
+console.log(add(5, 5));
